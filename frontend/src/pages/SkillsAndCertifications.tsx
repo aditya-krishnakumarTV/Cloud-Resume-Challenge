2e9020d00eabcf843bc40c3d2ee6c15254d34237
Aditya Krishnakumar <adityakrishnakumar12@gmail.com>
2025-10-31 19:53:15 +0530

    changes and css for skills page

--- frontend/src/pages/SkillsAndCertifications.tsx
+++ frontend/src/pages/SkillsAndCertifications.tsx
@@ -1,20 +1,31 @@
 function SkillsAndCertifications() {
-  const programming_languages = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "TypeScript",
-    "Python",
+  const skills = [
+    {
+      title: "Programming Languages",
+      content: ["HTML", "CSS", "JavaScript", "TypeScript", "Python"],
+    },
+    {
+      title: "Frameworks",
+      content: ["Angular", "React"],
+    },
+    {
+      title: "Styling Frameworks",
+      content: ["Bootstrap", "Angular Material", "TailwindCSS"],
+    },
+    {
+      title: "Cloud & DevOps",
+      content: [
+        "AWS Services",
+        "Terraform & CloudFormation",
+        "GitHub Actions (CI/CD)",
+        "Docker",
+      ],
+    },
+    {
+      title: "Certifications",
+      content: ["AWS Certified Cloud Practitioner"],
+    },
   ];
-  const frameworks = ["Angular", "React"];
-  const styling_frameworks = ["Bootstrap", "Angular Material", "TailwindCSS"];
-  const cloud_and_devops = [
-    "AWS Services",
-    "Terraform & CloudFormation",
-    "GitHub Actions (CI/CD)",
-    "Docker",
-  ];
-  const certifications = ["AWS Certified Cloud Practitioner"];
 
   return (
     <div className="bg-gray-100">
@@ -31,81 +42,23 @@ function SkillsAndCertifications() {
         <div className="flex flex-col md:flex-row">
           <div className="flex-1 p-8">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              <div className="bg-white p-6 rounded-lg shadow-lg">
-                <h2 className="text-2xl text-center font-semibold mb-4">
-                  Programming Languages
-                </h2>
-                <div className="flex flex-row flex-wrap gap-3 p-4">
-                  {programming_languages.map((lang) => (
-                    <div
-                      className="border rounded-2xl text-center w-max py-1 px-3"
-                      key={lang}
-                    >
-                      {lang}
-                    </div>
-                  ))}
+              {skills.map((skill) => (
+                <div className="bg-white p-6 rounded-lg transition hover:-translate-y-1 hover:shadow-lg shadow-gray-500 ease-in-out duration-400">
+                  <h2 className="text-2xl text-center font-semibold mb-4">
+                    {skill.title}
+                  </h2>
+                  <div className="flex flex-row flex-wrap gap-3 p-4">
+                    {skill.content.map((item) => (
+                      <div
+                        className="border rounded-2xl text-center w-max py-1 px-3 transition hover:scale-105 hover:shadow-md shadow-gray-400 ease-in-out duration-500"
+                        key={item}
+                      >
+                        {item}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-              <div className="bg-white p-6 rounded-lg shadow-lg">
-                <h2 className="text-2xl text-center font-semibold mb-4">
-                  Frameworks
-                </h2>
-                <div className="flex flex-row flex-wrap gap-3 p-4">
-                  {frameworks.map((fw) => (
-                    <div
-                      className="border rounded-2xl text-center w-max py-1 px-3"
-                      key={fw}
-                    >
-                      {fw}
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="bg-white p-6 rounded-lg shadow-lg">
-                <h2 className="text-2xl text-center font-semibold mb-4">
-                  Styling Frameworks
-                </h2>
-                <div className="flex flex-row flex-wrap gap-3 p-4">
-                  {styling_frameworks.map((sf) => (
-                    <div
-                      className="border rounded-2xl text-center w-max py-1 px-3"
-                      key={sf}
-                    >
-                      {sf}
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="bg-white p-6 rounded-lg shadow-lg">
-                <h2 className="text-2xl text-center font-semibold mb-4">
-                  Cloud & DevOps
-                </h2>
-                <div className="flex flex-row flex-wrap gap-3 p-4">
-                  {cloud_and_devops.map((cd) => (
-                    <div
-                      className="border rounded-2xl text-center w-max py-1 px-3"
-                      key={cd}
-                    >
-                      {cd}
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="bg-white p-6 rounded-lg shadow-md">
-                <h2 className="text-2xl text-center font-semibold mb-4">
-                  Certifications
-                </h2>
-                <div className="flex flex-row flex-wrap gap-3 p-4">
-                  {certifications.map((cert) => (
-                    <div
-                      className="border rounded-2xl text-center w-max py-1 px-3"
-                      key={cert}
-                    >
-                      {cert}
-                    </div>
-                  ))}
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
