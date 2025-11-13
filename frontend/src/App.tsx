8691b8b5c46054716b6de84d624d2214a3dd892b
Aditya Krishnakumar <adityakrishnakumar12@gmail.com>
2025-11-13 21:26:30 +0530

    Added gsap scroll animation for each page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,47 @@ import Home from "./pages/Home";
 import Info from "./pages/Info";
 import SkillsAndCertifications from "./pages/SkillsAndCertifications";
 import Contact from "./pages/Contact";
-import Footer from "./pages/Footer";
+
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  const pages = ["home", "info", "skills-and-certifications", "contact"];
+
+  useGSAP(() => {
+    pages.forEach((pageId) => {
+      gsap.from(`#${pageId}`, {
+        opacity: 0,
+        duration: 1,
+        ease: "power2.in",
+        scrollTrigger: {
+          trigger: `#${pageId}`,
+          start: "top 80%",
+          end: "bottom 20%",
+          toggleActions: "play none none none",
+        },
+      });
+    });
+  }, []);
+
   return (
-    <>
-      <Home />
-      <Info />
-      <SkillsAndCertifications />
-      <Contact />
-      <Footer />
-    </>
+    <main>
+      <div id="home">
+        <Home />
+      </div>
+      <div id="info">
+        <Info />
+      </div>
+      <div id="skills-and-certifications">
+        <SkillsAndCertifications />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+    </main>
   );
 }
 
