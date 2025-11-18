40475a8a898d92e5cb9e390ffeafc09464e4404d
Aditya Krishnakumar <adityakrishnakumar12@gmail.com>
2025-11-18 20:27:06 +0530

    Styling chages for info page

--- frontend/src/pages/Info.tsx
+++ frontend/src/pages/Info.tsx
@@ -1,26 +1,30 @@
 function Info() {
   return (
     <div className="bg-white">
-      <div className="max-w-7xl mx-auto p-10 scale-z-100">
+      <div className="max-w-7xl mx-auto p-10 min-h-screen">
         <div className="flex flex-col md:flex-row">
           <div className="flex-1 p-8">
+            <img
+              className="rounded-full w-100 shadow-lg shadow-gray-500"
+              src="/profilepic.jpg"
+            />
+          </div>
+          <div className="flex-1"></div>
+        </div>
+
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-1"></div>
+          <div className="flex-1 p-8 text-left">
             <div className="text-center md:text-left text-black space-y-4">
               <h1 className="text-4xl font-bold font-signature">About Me</h1>
               <p className="text-lg">
-                A results-driven Software Developer with over 3 years of
-                experience. I am adept at leveraging technical expertise to
-                achieve organizational and personal milestones. I am passionate
-                about continuous learning and proactively embrace new
-                technologies to deliver impactful and innovative solutions.
+                A results-driven Software Developer, adept at leveraging
+                technical expertise to achieve organizational and personal
+                milestones. I am passionate about continuous learning and
+                proactively embrace new technologies to deliver impactful and
+                innovative solutions.
               </p>
             </div>
-          </div>
-
-          <div className="flex-1 p-8">
-            <img
-              className="rounded-full w-95 mx-auto shadow-lg shadow-gray-500"
-              src="/profilepic.jpg"
-            />
           </div>
         </div>
       </div>
